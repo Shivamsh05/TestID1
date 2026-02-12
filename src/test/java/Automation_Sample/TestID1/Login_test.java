272fb5fba371bd56dad3b8dc8228c7003ac1b2a9
Shivam Malviya <malviyashivam08@gmail.com>
2026-02-12 23:58:07 +0530

    version-3

--- src/test/java/Automation_Sample/TestID1/Login_test.java
+++ src/test/java/Automation_Sample/TestID1/Login_test.java
@@ -2,19 +2,30 @@ package Automation_Sample.TestID1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 import Pages.Login_Page;
 
 public class Login_test {
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+    	
+//        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        
+        // Add the "--headless" argument
+        options.addArguments("--headless");
         driver.manage().window().maximize();
 
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         Login_Page loginpage = new Login_Page(driver); 
         loginpage.login();
+        Thread.sleep(5000);
+        loginpage.logout();
         Thread.sleep(5000);
 
         driver.quit();
--- src/test/java/Pages/Login_Page.java
+++ src/test/java/Pages/Login_Page.java
@@ -1,40 +1,89 @@
 package Pages;
 
+import java.util.List;
 import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+//For soft assertions
+import org.testng.asserts.SoftAssert;
+
+import Automation_Sample.TestID1.Login_test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Login_Page {
 
     WebDriver driver;
     WebDriverWait wait;
-
+    private static final Logger logger = LogManager.getLogger(Login_test.class);
+    
+    
+    
     By Username = By.xpath("//input[@name='username']");
     By Password = By.xpath("//input[@placeholder='Password']");
     By LoginButton = By.xpath("//button[@type='submit']");
+    By profileMenu = By.xpath("//span[@class='oxd-userdropdown-tab']");
+    By LogoutButton = By.xpath("//ul[@class='oxd-dropdown-menu']");
+
 
     public Login_Page(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
-
+    @Test
     public void login() {
     	try {
         wait.until(ExpectedConditions.visibilityOfElementLocated(Username)).sendKeys("Admin");
     	}catch(Exception e) {
-    		
+    		 logger.error("Error occurred", e);
     	}
     	try {
         wait.until(ExpectedConditions.visibilityOfElementLocated(Password)).sendKeys("admin123");
         }catch(Exception e) {
-        	
+        	 logger.error("Error occurred", e);
         }
     	try {
         wait.until(ExpectedConditions.elementToBeClickable(LoginButton)).click();
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM", "Page title mismatch"); 
+        logger.info("User logged in successfully..");
+        System.out.println("Login work..");
+        System.out.println(System.getProperty("log4j.configurationFile"));
     	}catch(Exception e) {
-    		
+    		 logger.error("Error occurred", e);
     	}
+    }
+    @Test
+    public void logout() throws InterruptedException {
+    	//To be continue....
+    	
+    	try {
+    		wait.until(ExpectedConditions.elementToBeClickable(profileMenu)).click();
+    		}catch(Exception e) {
+    		System.out.println("Profile menu not found"+e);
+    		 logger.error("Error occurred", e);
+    		}
+    	try {
+    		Thread.sleep(5000);
+//        	wait.until(ExpectedConditions.elementToBeClickable(LogoutButton)).click();
+    		List<WebElement> list = driver.findElements(LoginButton);
+    		for (WebElement i : list) {
+    		    if (i.getText().equals("Logout")) {
+    		    	System.out.println(i);
+    		        i.click();
+    		        break;
+    		    }
+    		}
+        	logger.info("User logged out successfully..");
+        	}catch(Exception e) {
+        		System.out.println("Logout button not found"+e);
+        		 logger.error("Error occurred", e);
+        	}
     }
 }
